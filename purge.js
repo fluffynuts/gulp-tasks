ef078dec5c0bef73f0a643f6a4294ffb4a454a05
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-09-20 15:33:49 +0200

    feat: ignore binaries under node_modules too

--- purge.js
+++ purge.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
-var ignore = require('gulp-ignore');
 var purge = requireModule('gulp-purge');
 gulp.task('purge', function() {
-    return gulp.src(['source/**/bin/**', 'source/**/obj/**'])
-                .pipe(ignore([
-                            'source/**/packages/**',
-                            'source/**/node_modules/**',
-                            'node_modules/**'
-                ]))
+    return gulp.src(['./source/**/bin/**', 
+                     './source/**/obj/**', 
+                     '!./**/node_modules/**',
+                     '!./source/**/packages/**'])
                 .pipe(purge({
                     dryRun: false,
                     debug: false
