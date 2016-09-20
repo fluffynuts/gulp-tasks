1a5081f1d3981070293145c4c9020e8a127afeba
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-09-20 15:30:45 +0200

    feat: properly fail when a sub-task fails

--- default.js
+++ default.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var runSequence = require('run-sequence');
 
-gulp.task('default', function(cb) {
+gulp.task('default', function() {
     runSequence('purge', 'git-submodules', 'build', 'cover-dotnet', function(err) {
+      return new Promise(function(resolve, reject) {
         if (err) {
             gutil.log(gutil.colors.red(gutil.colors.bold(err)));
+            reject(err);
+        } else {
+          resolve();
         }
-        cb();
+      });
     });
 });
 
