701a07b9c4f39dbf436e9da39833df57230f6772
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-09-20 15:34:30 +0200

    feat: add module for running an NPM script in some folder under your build root

---
+++ modules/npm-run.js
@@ -0,0 +1,33 @@
+var child_process = require('child_process'),
+    which = require('which'),
+    fs = require('fs');
+
+function runNpmScript(target, folder, resolve, reject) {
+  var proc = child_process.spawn(which.sync('npm'), ['run', target], {
+    cwd: folder,
+    stdio: 'inherit'
+  });
+  proc.on('close', function(code) {
+    if (code) {
+      reject(code);
+    }
+    resolve(code);
+  });
+}
+
+module.exports = function(target, folder) {
+  return new Promise(function(resolve, reject) {
+    if (!folder) {
+      folder = '.';
+    }
+    if (!fs.existsSync(folder) && fs.existsSync(target)) {
+      var swap = target;
+      target = folder;
+      folder = swap;
+    }
+    if (!fs.existsSync(folder)) {
+      reject('Unable to run npm script: folder not found: "' + folder + '"');
+    }
+    return runNpmScript(target, folder, resolve, reject);
+  });
+}
