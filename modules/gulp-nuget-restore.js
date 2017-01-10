1a06977a901980fcb82a365ecbd0dc77bd98d00e
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2017-01-10 12:58:46 +0200

    feat: extract out find-local-nuget so that other utilities         may perform nuget functions without having to know         where nuget is

--- modules/gulp-nuget-restore.js
+++ modules/gulp-nuget-restore.js
@@ -8,7 +8,7 @@ var exec = require('./exec');
 var log = require('./log');
 var path = require('path');
 var which = require('which');
-var HttpDownloader = require('./http-downloader');
+var findLocalNuget = require('./find-local-nuget');
 
 var PLUGIN_NAME = 'gulp-dotcover';
 var DEBUG = true;
@@ -65,65 +65,6 @@ function writeStatus(str) {
 
 function humanSize(size) {
     return (size / 1024 / 1024).toFixed(2) + 'mb';
-}
-
-function completedFile(path) {
-    return path + '.completed';
-}
-
-function wasCompletedWithinADay(completed, nuget) {
-    var completedExists = fs.existsSync(completed);
-    var nugetExists = fs.existsSync(nuget);
-    if (!completedExists || !nugetExists) {
-      if (completedExists) {
-        fs.unlinkSync(completed);
-      }
-      if (nugetExists) {
-        fs.unlinkSync(nuget);
-      }
-      return false;
-    }
-    try {
-        var ms = Date.parse(fs.readFileSync(completed).toString());
-        if (isNaN(ms)) {
-            return false;
-        }
-        var completedAt = new Date(ms),
-            completedDelta = (new Date()) - completedAt;
-        return completedDelta < 86400000;   // one day. arb.
-    } catch (ignore) {
-        return false;
-    }
-}
-
-function findLocalNuget() {
-    var localNuget = path.join(__dirname, 'nuget.exe'),
-        completed = completedFile(localNuget),
-        url = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe';
-    var i = 1;
-    return new Promise(function(resolve, reject) {
-        if (wasCompletedWithinADay(completed, localNuget)) {
-            return resolve(localNuget);
-        }
-        var lastCompleted = null;
-        if (fs.existsSync(completed) && fs.existsSync(localNuget)) {
-            lastCompleted = fs.readFileSync(completed);
-            fs.unlinkSync(completed);
-        }
-        var downloader = new HttpDownloader();
-        downloader.download(url, localNuget).then(function(dl) {
-            fs.writeFileSync(completed, new Date());
-            resolve(dl);
-        }).catch(function(err) {
-            if (lastCompleted) {
-                fs.writeFileSync(completed, lastCompleted);
-                log.info(err);
-                log.info('Falling back on last local nuget.exe');
-                return resolve(localNuget);
-            }
-            reject(err);
-        });
-    });
 }
 
 function checkIfNugetIsAvailable(nugetPath, stream) {
