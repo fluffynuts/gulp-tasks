255a4f0ed4037b112609a808c709a8c77865806a
Davyd McColl <davyd.mccoll@derivco.co.za>
2017-03-02 12:30:53 +0200

    reformat: minor

--- build.js
+++ build.js
@@ -2,17 +2,19 @@ var
   gulp = requireModule('gulp-with-help'),
   msbuild = require('gulp-msbuild');
 
-gulp.task('build', 'Builds all Visual Studio solutions in tree', ['nuget-restore'], function() {
-    return gulp.src(['**/*.sln', '!**/node_modules/**/*.sln'])
-            .pipe(msbuild({
-                toolsVersion: 14.0,
-                targets: ['Clean', 'Build'],
-                configuration: 'Debug',
-                stdout: true,
-                verbosity: 'minimal',
-                errorOnFail: true,
-                architecture: 'x64'
-            }));
+gulp.task('build', 'Builds all Visual Studio solutions in tree', ['nuget-restore'], function () {
+  return gulp.src([
+    '**/*.sln',
+    '!**/node_modules/**/*.sln'
+  ]).pipe(msbuild({
+      toolsVersion: 14.0,
+      targets: ['Clean', 'Build'],
+      configuration: 'Debug',
+      stdout: true,
+      verbosity: 'minimal',
+      errorOnFail: true,
+      architecture: 'x64'
+  }));
 });
 
 
