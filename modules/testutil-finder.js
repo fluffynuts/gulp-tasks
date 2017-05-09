063f76ad7f09dfb6a7799ae6b6dabab88e4ffd3d
Davyd McColl <davydm@gmail.com>
2017-05-09 11:34:59 +0200

    fix: back out argument deconstruction: not supported in node < 6

--- modules/testutil-finder.js
+++ modules/testutil-finder.js
@@ -43,9 +43,13 @@ function finder(searchBaseFolders, searchBaseSubFolder, searchFolderPrefix, sear
     searchBaseFolders = [programFilesFolder];
   }
   [searchFolderPrefix, searchBin, options] = args.slice(args.length - 3);
-  const ignoreBetas = options.ignoreBetas === undefined ? true : options.ignoreBetas;
-
-  const lprefix = searchFolderPrefix.toLowerCase();
+  const
+    someArgs = args.slice(args.length - 3),
+    searchFolderPrefix = args[0],
+    searchBin = args[1],
+    options = args[2],
+    ignoreBetas = options.ignoreBetas === undefined ? true : options.ignoreBetas,
+    lprefix = searchFolderPrefix.toLowerCase();
   const runner = searchBaseFolders
     .filter(checkExists)
     .reduce((possibles, baseFolder) => {
