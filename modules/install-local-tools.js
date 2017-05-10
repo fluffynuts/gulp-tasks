d8495dc9884ff48305ca8d34b0c9aa75b02d3a63
Davyd McColl <davyd.mccoll@derivco.co.za>
2017-05-10 10:59:39 +0200

    feat: attempt nuget self-update if already exists instead of fresh download

--- modules/install-local-tools.js
+++ modules/install-local-tools.js
@@ -1,5 +1,6 @@
 const
   downloadNuget = require("./download-nuget"),
+  gutil = require("gulp-util"),
   exec = requireModule("exec"),
   path = require("path"),
   fs = require("fs"),
@@ -7,11 +8,11 @@ const
   nugetExe = "nuget.exe",
   del = require("del");
 
-function ensureFolderExists() {
+function ensureFolderExists(toolsFolder) {
   return new Promise((resolve, reject) => {
     try {
-      if (!fs.existsSync(defaultToolsFolder)) {
-        fs.mkdirSync(defaultToolsFolder);
+      if (!fs.existsSync(toolsFolder)) {
+        fs.mkdirSync(toolsFolder);
       }
       resolve();
     } catch (e) {
@@ -20,14 +21,26 @@ function ensureFolderExists() {
   });
 }
 
-function cleanFoldersFrom() {
-  const dirs = fs.readdirSync(defaultToolsFolder)
-    .map(p => path.join(defaultToolsFolder, p))
+function cleanFoldersFrom(toolsFolder) {
+  const dirs = fs.readdirSync(toolsFolder)
+    .map(p => path.join(toolsFolder, p))
     .filter(p => {
       const stat = fs.lstatSync(p);
       return stat.isDirectory();
     });
   return del(dirs);
+}
+
+function downloadOrUpdateNuget(targetFolder) {
+  const nugetPath = path.join(targetFolder, "nuget.exe");
+  if (fs.existsSync(nugetPath)) {
+    gutil.log("nuget.exe already exists... attempting self-update");
+    return exec(nugetPath, [
+      "update", "-self"
+    ]);
+  } else {
+    return downloadNuget(targetFolder);
+  }
 }
 
 module.exports = (requiredTools, overrideToolsFolder) => {
@@ -40,7 +53,7 @@ module.exports = (requiredTools, overrideToolsFolder) => {
   const targetFolder = overrideToolsFolder || defaultToolsFolder;
   return ensureFolderExists(targetFolder)
           .then(() => cleanFoldersFrom(targetFolder))
-          .then(() => downloadNuget(targetFolder))
+          .then(() => downloadOrUpdateNuget(targetFolder))
           .then(() => Promise.all(
             requiredTools.map(tool => exec(
               nugetExe,
