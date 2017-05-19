db9dbfe53764dba869b44a1e9b16725d8459a30b
Davyd McColl <davydm@gmail.com>
2017-05-19 15:55:04 +0200

    fix: gutil.error is not a thing

--- default.js
+++ default.js
@@ -6,8 +6,7 @@ gulp.task('default', 'Purges, updates git submodules, builds, covers dotnet', fu
   runSequence('purge', 'build', 'cover-dotnet', function (err) {
     return new Promise(function (resolve, reject) {
       if (err) {
-        console.error(err);
-        gutil.error(gutil.colors.red(gutil.colors.bold(err)));
+        gutil.log(gutil.colors.red(gutil.colors.bold(err)));
         reject(err);
       } else {
         resolve();
