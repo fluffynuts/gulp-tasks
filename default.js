decbbcfbd4729a776446d043fa10e6500e0a670c
Davyd McColl <davydm@gmail.com>
2017-05-19 16:27:25 +0200

    feat: add shell 'generate-reports' task and add it to the end of the default pipeline

--- default.js
+++ default.js
@@ -1,9 +1,9 @@
-var gulp = requireModule('gulp-with-help'),
-  gutil = require('gulp-util'),
-  runSequence = require('run-sequence');
+var gulp = requireModule("gulp-with-help"),
+  gutil = require("gulp-util"),
+  runSequence = require("run-sequence");
 
-gulp.task('default', 'Purges, updates git submodules, builds, covers dotnet', function () {
-  runSequence('purge', 'build', 'cover-dotnet', function (err) {
+gulp.task("default", "Purges, updates git submodules, builds, covers dotnet", function () {
+  runSequence("purge", "build", "cover-dotnet", "generate-reports", function (err) {
     return new Promise(function (resolve, reject) {
       if (err) {
         gutil.log(gutil.colors.red(gutil.colors.bold(err)));
---
+++ generate-reports.js
@@ -0,0 +1,6 @@
+var gulp = requireModule("gulp-with-help");
+
+gulp.task(
+"generate-reports",
+"Shell task to generate reports at the end of your build -- override in your local-tasks",
+done => done());
