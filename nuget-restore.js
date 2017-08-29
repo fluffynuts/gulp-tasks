b24f28af08637f66ecc03cbfe2a7668be266ace1
Davyd McColl <davydm@gmail.com>
2017-08-29 21:50:15 +0200

    refactor: remove dead code

--- nuget-restore.js
+++ nuget-restore.js
@@ -6,23 +6,6 @@ var gulp = requireModule("gulp-with-help"),
   findLocalNuget = requireModule("find-local-nuget"),
   resolveNuget = requireModule("./resolve-nuget");
 
-function createNugetRestoreTask() {
-  var deps = [];
-  try {
-    resolveNuget();
-  } catch (ignore) {
-    debug("should download nuget");
-    deps.push("get-local-nuget");
-  }
-  gulp.task("nuget-restore", "Restores all nuget packages in all solutions", deps, function () {
-    return gulp.src(["**/*.sln", "!**/node_modules/**/*.sln", "!./tools/**/*.sln"])
-      .pipe(nugetRestore({
-        debug: false
-      }));
-  });
-}
-
-// createNugetRestoreTask();
 gulp.task("nuget-restore",
   "Restores all nuget packages in all solutions",
   ["install-tools"], () => {
