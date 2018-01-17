0a44598646f036ed15e3625c314bcb0e9fc200fa
davyd.mccoll@codeo.co.za <davyd.mccoll@codeo.co.za>
2018-01-17 10:01:35 +0200

    fix: better http-downloader, using streaming. More reliable, faster, less code

--- modules/http-downloader.js
+++ modules/http-downloader.js
@@ -1,33 +1,36 @@
 var fs = require('fs'),
-    request = require('request'),
-    debug = require('debug')('http-downloader');
+  request = require('request'),
+  debug = require('debug')('http-downloader');
+
 function HttpDownloader(infoLogFunction, debugLogFunction) {
   this._info = infoLogFunction || console.log;
-  this._debug = debugLogFunction || function () { };
+  this._debug = debugLogFunction || debug;
   this.assumeDownloadedIfExistsAndSizeMatches = true;
 }
 
 HttpDownloader.prototype = {
   download: function (url, target) {
-    if (this._request) {
-      throw new Error('Already downloading.');
-    }
-    this._errored = false;
-    var result = this._createResultPromise();
-    this._url = url;
-    this._target = target;
-    this._tempTarget = target + '.part';
-    var self = this;
-    this._info('Start download of "' + url + '"');
-    this._request = request.get(this._url, { timeout: 30000 })
-      .on('response', function (response) {
-        self._response = response;
-        self._handleResponse();
-      })
-      .on('error', function (e) {
-        self._reject('Download failed: ' + (e || 'unknown error'));
-      });
-    return result;
+    const partFile = `${target}.part`;
+    return new Promise((resolve, reject) => {
+      this._request = request.get(url, { timeout: 30000 })
+        .on("response", (response) => {
+          this._debug(`got response: ${JSON.stringify(response)}`);
+          this._downloadSize = parseInt(response.headers["content-length"]);
+          this._statusSuffix = '% of ' + this._humanSize(this._downloadSize);
+        })
+        .on("error", (e) => {
+          this._debug(`got error: ${e}`);
+          reject(e);
+        })
+        .on("data", (data) => {
+          this._debug(`got ${data.length} bytes`);
+          this._updateStatus(data);
+        })
+        .on("end", () => {
+          this._clear();
+          this._rename(resolve, reject, partFile, target);
+        }).pipe(fs.createWriteStream(partFile));
+    });
   },
   abort: function () {
     if (this._request) {
@@ -36,36 +39,11 @@ HttpDownloader.prototype = {
       this._clear();
     }
   },
-  _handleResponse: function () {
-    this._downloadSize = parseInt(this._response.headers['content-length']);
-    if (this._alreadyDownloaded()) {
-      this._request.destroy();
-      return this._resolve(this._target);
-    }
-    if (fs.existsSync(this._target)) {
-      fs.unlinkSync(this._target);
-    }
-    this._statusSuffix = '% of ' + this._humanSize(this._downloadSize);
-    this._bindOnResponsedata();
-    this._bindOnResponseEnd();
-    this._bindOnResponseError();
-  },
-  _bindOnResponsedata: function () {
-    this._lastData = new Date();
-    this._written = 0;
-    var self = this;
-    this._response.on('data', function (data) {
-      self._lastData = new Date();
-      self._errored = false;
-      self._debug('writing ' + data.length + ' bytes to ' + self._tempTarget);
-      fs.appendFileSync(self._tempTarget, data);
-      self._updateStatus(data);
-    });
-  },
   _updateStatus: function (data) {
     if (process.env.SUPPRESS_DOWNLOAD_PROGRESS) {
       return;
     }
+    this._written = this._written || 0;
     this._written += data.length;
     var perc = Math.round((100 * this._written) / this._downloadSize);
     if (perc != this._lastPerc) {
@@ -77,59 +55,23 @@ HttpDownloader.prototype = {
     this._clearStatus();
     process.stdout.write(msg);
   },
-  _bindOnResponseEnd: function () {
-    var self = this;
-    this._response.on('end', function () {
-      if (self._errored) {
-        return;
-      }
-      self._clearStatus();
-      self._rename(self._tempTarget, self._target);
-      self._clear();
-    });
-  },
-  _rename: function (src, dst, attempts) {
+  _rename: function (resolve, reject, src, dst, attempts) {
     try {
-      debug('attempt rename of temp file');
+      this._debug('attempt rename of temp file');
       fs.renameSync(src, dst)
       this._clearStatus();
       this._info('-> download complete!');
-      this._resolve(dst)
+      resolve(dst)
     } catch (e) {
-      debug('rename error:', e);
+      this._debug('rename error:', e);
       if (attempts > 99) {
-        this._reject(['Unable to rename "', src, '" to "', ds, '": ', e].join(''));
+        reject(['Unable to rename "', src, '" to "', ds, '": ', e].join(''));
       } else {
-        var self = this;
-        setTimeout(function () {
-          self._rename(src, dst, attempts++);
+        setTimeout(()=> {
+          this._rename(resolve, reject, src, dst, attempts++);
         }, 100);
       }
     }
-  },
-  _bindOnResponseError: function () {
-    var self = this;
-    this._response.on('error', function (err) {
-      self._errored = true;
-      self._handleDownloadError(err);
-    });
-  },
-  _handleDownloadError: function (err) {
-    this._reject([
-      'Download of "',
-      this._url,
-      '" failed: \n',
-      this._errorStringFor(err)
-    ]);
-  },
-  _errorStringFor: function (err) {
-    if (!err) {
-      return 'Unknown error';
-    }
-    if (typeof (err.toString) === 'function') {
-      return err.toString();
-    }
-    return '' + err;
   },
   _clearStatus: function () {
     process.stdout.write('\r               \r');
@@ -143,21 +85,6 @@ HttpDownloader.prototype = {
       size /= 1024;
     }
     return size.toFixed(2) + suffixes[suffixes.length - 1];
-  },
-  _createResultPromise: function () {
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self._resolveFunction = resolve;
-      self._rejectFunction = reject;
-    });
-  },
-  _resolve: function (data) {
-    debug('resolving final promise!');
-    this._resolveFunction(data);
-  },
-  _reject: function (data) {
-    debug('rejecting final promise!');
-    this._rejectFunction(data);
   },
   _clear: function () {
     var self = this;
