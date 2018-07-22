1982995d1cb183528d75ed5d6ce856192e15776d
Davyd McColl <davydm@gmail.com>
2018-07-22 17:18:39 +0200

    fix: typo in suggestion about default report generator

--- generate-reports.js
+++ generate-reports.js
@@ -2,5 +2,5 @@ var gulp = requireModule("gulp-with-help");
 
 gulp.task(
 "generate-reports",
-"Shell task: override in local-tasks (try depend on 'generate-default-reports')",
+"Shell task: override in local-tasks (try depend on 'default-report-generator')",
 done => done());
