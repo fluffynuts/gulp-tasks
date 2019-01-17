2f33925f41a6a469da2e8ed4ed1509972ef544df
Davyd McColl <davydm@gmail.com>
2019-01-17 11:13:38 +0200

    hack: attempt to run nuget.exe before resolving as it may be locked by, eg, virus scanner

--- modules/download-nuget.js
+++ modules/download-nuget.js
@@ -1,11 +1,54 @@
 const
   HttpDownloader = require("./http-downloader"),
+  exec = require("./exec"),
+  logger = require("./log"),
   path = require("path"),
   url = 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe';
 
 function downloadNugetTo(targetFolder) {
-  const downloader = new HttpDownloader();
-  return downloader.download(url, path.join(targetFolder, "nuget.exe"));
+  logger.debug(`Attempting to download nuget.exe to ${targetFolder}`);
+  const
+    downloader = new HttpDownloader(),
+    target = path.join(targetFolder, "nuget.exe");
+  return downloader
+    .download(url, path.join(targetFolder, "nuget.exe"))
+    .then(() => validateCanRunExe(target));
+}
+
+function validateCanRunExe(exePath) {
+  return new Promise((resolve, reject) => {
+    var
+      lastMessage = "unknown error",
+      attempts = 0;
+    setTimeout(function testExe() {
+      if (attempts === 10) {
+        return reject(`Unable to run executable at ${exePath}: ${lastMessage}`);
+      }
+      attempts++;
+      try {
+        logger.debug(`attempt #${attempts} to run ${exePath}`);
+        exec(exePath, ["update", "-self"]);
+        logger.info(`nuget.exe appears to be valid!`);
+        return resolve();
+      } catch (e) {
+        lastMessage = e.message || lastMessage;
+        logger.debug(`failed to run executable (${e.message}); ${i < 9 ? "will try again" : "giving up"}`);
+      }
+
+      setTimeout(testExe, 1000);
+    }, 1000);
+
+    for (var i = 0; i < 10; i++) {
+      try {
+        exec(exePath, ["update", "-self"]);
+        resolve();
+      } catch (e) {
+        lastMessage = e.message || lastMessage;
+        logger.debug(`failed to run executable (${e.message}); ${i < 9 ? "will try again" : "giving up"}`);
+      }
+    }
+    reject(`Unable to run executable at ${exePath}: ${lastMessage}`);
+  });
 }
 
 function retry(fn, attempt, maxAttempts, wait) {
--- modules/log.js
+++ modules/log.js
@@ -1,10 +1,17 @@
-var gutil = require('gulp-util');
-var DEBUG = 1;
-var INFO = 2;
-var NOTICE = 3;
-var WARNING = 4;
-var ERROR = 5;
-var LogLevels = function() {
+var
+    gutil = require('gulp-util'),
+    DEBUG = 1,
+    INFO = 2,
+    NOTICE = 3,
+    WARNING = 4,
+    ERROR = 5,
+    levels = {
+        DEBUG,
+        INFO,
+        NOTICE,
+        WARNING
+    };
+var LogLevels = function () {
 };
 LogLevels.prototype = {
     get Debug() {
@@ -23,75 +30,86 @@ LogLevels.prototype = {
         return ERROR;
     }
 };
-var Logger = function() {
+var Logger = function () {
     this.LogLevels = new LogLevels();
-    this.setThreshold(INFO);
+    var logLevel = (process.env.LOG_LEVEL || "").toUpperCase();
+    this.setThreshold(levels[logLevel] || INFO);
     this._timestamp = true;
 };
 Logger.prototype = {
-    setThreshold: function(value) {
+    get threshold() {
+        return this._threshold;
+    },
+    setThreshold: function (value) {
         var intValue = parseInt(value);
         if (isNaN(intValue) || intValue < 1 || intValue > 5) {
             throw value + ' is not a valid integer value. Try use one of (logger).LogLevels.{Debug|Info|Notice|Warning|Error}';
         }
         this._threshold = intValue;
     },
-    debug: function(message) {
+    debug: function (message) {
         if (this._threshold > DEBUG)
             return;
         this._print(message, 'grey');
     },
-    info: function(message) {
+    info: function (message) {
         if (this._threshold > INFO)
             return;
         this._print(message, 'yellow');
     },
-    warning: function(message) {
+    warning: function (message) {
         if (this._threshold > WARNING)
             return;
         this._print(message, 'magenta');
     },
-    error: function(message) {
+    error: function (message) {
         this._print(message, 'red', 'bold');
     },
-    fail: function(message) {
+    fail: function (message) {
         this.error(message);
     },
-    ok: function(message) {
+    ok: function (message) {
         this._print(message, 'green');
     },
-    notice: function(message) {
+    notice: function (message) {
         if (this._threshold > NOTICE)
             return;
         this._print(message, 'cyan');
     },
-    suppressTimeStamps: function() {
-      this._timestamp = false;
+    suppressTimeStamps: function () {
+        this._timestamp = false;
     },
-    showTimeStamps: function() {
-      this._timestamp = true;
+    showTimeStamps: function () {
+        this._timestamp = true;
     },
-    _print: function() {
+    _print: function () {
         var message = arguments[0];
         var styles = [];
         for (var i = 1; i < arguments.length; i++) {
             styles.push(arguments[i]);  // because arguments is an object, not an array...
         }
-        var styleFunction = styles.reduce(function(acc, cur) {
+        var styleFunction = styles.reduce(function (acc, cur) {
             var fn = gutil.colors[cur];
             if (fn === undefined) {
                 return acc;
             }
-            return function(s) {
+            return function (s) {
                 return fn(acc(s));
             };
-        }, function(s) { return s; });
+        }, function (s) { return s; });
         if (this._timestamp) {
-          gutil.log(styleFunction(message));
+            gutil.log(styleFunction(message));
         } else {
-          console.log(styleFunction(message));
+            console.log(styleFunction(message));
         }
     }
 };
 var logger = new Logger();
+if (logger.threshold === DEBUG) {
+    logger.debug(" -- testing logger outputs -- ");
+    logger.debug("debug message");
+    logger.info("info message");
+    logger.warning("warning message");
+    logger.error("error message");
+}
 module.exports = logger;
