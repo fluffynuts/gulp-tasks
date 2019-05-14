bb25a14dc7720ce2029a0099673f131bf10b1e11
Davyd McColl <davydm@gmail.com>
2019-05-14 16:33:17 +0200

    :loud_sound: up the log verbosity so that each test is logged as it is run

--- test-dotnet.js
+++ test-dotnet.js
@@ -1,39 +1,58 @@
 var gulp = requireModule("gulp-with-help"),
   fs = require("fs"),
   areAllDotnetCore = requireModule("are-all-dotnet-core"),
+  promisifyStream = requireModule("promisify-stream"),
   os = require("os"),
   { test } = require("gulp-dotnet-cli"),
   nunit = require("gulp-nunit-runner"),
   getToolsFolder = requireModule("get-tools-folder"),
   testUtilFinder = requireModule("testutil-finder");
 
-gulp.task("test-dotnet",
+gulp.task(
+  "test-dotnet",
   "Runs all tests in your solution via NUnit",
-  [ "build" ],
+  ["build"],
   async () => {
-  if (!fs.existsSync("buildreports")) {
-    fs.mkdirSync("buildreports");
-  }
-
-  const projects = [ "**/*.Tests.csproj", "**/Tests.csproj", "**/Test.csproj", "!**/node_modules/**/*.csproj", `!./${getToolsFolder}/**/*.csproj`];
-  if (await areAllDotnetCore(projects)) {
-    return gulp.src(projects)
-      .pipe(test());
-  }
-
-  var agents = parseInt(process.env.MAX_NUNIT_AGENTS);
-  if (isNaN(agents)) {
-    agents = os.cpus().length - 1;
-  }
-  return gulp.src([
-    "**/bin/Debug/**/*.Tests.dll",
-    "**/bin/*.Tests.dll"
-  ], { read: false }).pipe(nunit({
-    executable: testUtilFinder.latestNUnit({ architecture: "x86" }),
-    options: {
-      result: "buildreports/nunit-result.xml",
-      agents: agents,
-      labels: "All"
+    if (!fs.existsSync("buildreports")) {
+      fs.mkdirSync("buildreports");
     }
-  }));
-});
+
+    const projects = [
+      "**/*.Tests.csproj",
+      "**/Tests.csproj",
+      "**/Test.csproj",
+      "!**/node_modules/**/*.csproj",
+      `!./${getToolsFolder}/**/*.csproj`
+    ];
+    if (await areAllDotnetCore(projects)) {
+      return promisifyStream(
+        gulp.src(projects).pipe(
+          test({
+            verbosity: "normal"
+          })
+        )
+      );
+    }
+
+    var agents = parseInt(process.env.MAX_NUNIT_AGENTS);
+    if (isNaN(agents)) {
+      agents = os.cpus().length - 1;
+    }
+    return promisifyStream(
+      gulp
+        .src(["**/bin/Debug/**/*.Tests.dll", "**/bin/*.Tests.dll"], {
+          read: false
+        })
+        .pipe(
+          nunit({
+            executable: testUtilFinder.latestNUnit({ architecture: "x86" }),
+            options: {
+              result: "buildreports/nunit-result.xml",
+              agents: agents,
+              labels: "All"
+            }
+          })
+        )
+    );
+  }
+);
