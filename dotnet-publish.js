194bb25b97b912d5d097e364a5902136b03a121b
Davyd McColl <davydm@gmail.com>
2019-06-26 20:21:38 +0200

    :sparkles: facilitate dotnet publish to exe

--- dotnet-publish.js
+++ dotnet-publish.js
@@ -1,17 +1,25 @@
-const
-  { publish } = require("gulp-dotnet-cli"),
-  gulp = requireModule("gulp-with-help")
+const { publish } = require("gulp-dotnet-cli"),
+  gulp = requireModule("gulp-with-help");
 
-gulp.task("dotnet-publish",
+gulp.task(
+  "dotnet-publish",
   "Performs `dotnet publish` on all non-test projects in the tree",
   () => {
-    return gulp.src([
-      "**/*.csproj",
-      "!**/Test.csproj",
-      "!**/*.Test.csproj",
-      "!**/*.Tests.csproj"
-    ])
-    .pipe(publish({
+    var publishOpts = {
       configuration: process.env.BUILD_CONFIGURATION || "Release"
-    }));
- });
+    };
+    if (process.env.DOTNET_PUBLISH_RUNTIMES) {
+      publishOpts.runtime = process.env.DOTNET_PUBLISH_RUNTIMES
+    }
+    return gulp
+      .src([
+        "**/*.csproj",
+        "!**/Test.csproj",
+        "!**/*.Test.csproj",
+        "!**/*.Tests.csproj"
+      ])
+      .pipe(
+        publish(publishOpts)
+      );
+  }
+);
