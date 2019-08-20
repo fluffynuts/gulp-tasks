91f9415df8ebf3ac775458b9317f0c813d1d904d
Davyd McColl <davydm@gmail.com>
2019-08-20 16:01:55 +0200

    :bug: fixes for node < 12 (await requires async)

--- modules/git-push.js
+++ modules/git-push.js
@@ -3,7 +3,7 @@ const
   gutil = require("gulp-util"),
   git = new Git();
 
-module.exports = function gitPush() {
+module.exports = async function gitPush() {
     gutil.log(gutil.colors.green("pushing local commits..."));
     await git.push(
       process.env.GIT_REMOTE || "origin",
