80c683d44a6b759097309209005fc1aa3c12ba14
Davyd McColl <davydm@gmail.com>
2019-09-03 10:22:17 +0200

    :loud_sound: log spawn failures

--- modules/spawn.js
+++ modules/spawn.js
@@ -1,16 +1,15 @@
 // use for spawning actual processes.
 // You must use exec if you want to run batch files
-var
-  tryLoadDebug = function() {
+var tryLoadDebug = function() {
     try {
       return require("debug")("spawn");
     } catch (e) {
-      return function() { };
+      return function() {};
     }
   },
   quoteIfRequired = require("./quote-if-required"),
   debug = tryLoadDebug(),
-  child_process = require('child_process');
+  child_process = require("child_process");
 
 var defaultOptions = {
   stdio: [process.stdin, process.stdout, process.stderr],
@@ -18,11 +17,12 @@ var defaultOptions = {
   shell: true
 };
 
-function spawn (executable, args, opts) {
+function spawn(executable, args, opts) {
   args = args || [];
   opts = Object.assign({}, defaultOptions, opts);
 
-  var stdOutWriter = null, stdErrWriter = null;
+  var stdOutWriter = null,
+    stdErrWriter = null;
   if (typeof opts.stdout === "function") {
     stdOutWriter = opts.stdout;
     opts.stdio[1] = "pipe";
@@ -37,32 +37,42 @@ function spawn (executable, args, opts) {
     args: args
   };
 
-  debug(`spawning: ${quoteIfRequired(executable)} ${args.map(a => '"' + a + '"').join(' ')}`);
+  executable = quoteIfRequired(executable);
+
+  debug(`spawning: ${executable} ${args.map(a => '"' + a + '"').join(" ")}`);
   debug({ opts });
 
   return new Promise((resolve, reject) => {
-    var child = child_process.spawn(quoteIfRequired(executable), args, opts);
-    child.on('error', function (err) {
-      debug(`child error: ${err}`);
-      result.error = err;
-      reject(`"${[executable].concat(args).join(' ')}" failed with "${err}"`);
-    });
-    child.on('close', function (code) {
-      debug(`child exits: ${code}`);
-      result.exitCode = code;
-      if (code === 0) {
-        resolve(result);
-      } else {
-        reject(`"${[executable].concat(args).join(' ')}" failed with exit code ${code}`);
+    try {
+      var child = child_process.spawn(executable, args, opts);
+      child.on("error", function(err) {
+        debug(`child error: ${err}`);
+        result.error = err;
+        reject(`"${[executable].concat(args).join(" ")}" failed with "${err}"`);
+      });
+      child.on("close", function(code) {
+        debug(`child exits: ${code}`);
+        result.exitCode = code;
+        if (code === 0) {
+          resolve(result);
+        } else {
+          reject(
+            `"${[executable]
+              .concat(args)
+              .join(" ")}" failed with exit code ${code}`
+          );
+        }
+      });
+      if (stdOutWriter) {
+        child.stdout.on("data", stdOutWriter);
       }
-    });
-    if (stdOutWriter) {
-      child.stdout.on("data", stdOutWriter);
-    }
-    if (stdErrWriter) {
-      child.stderr.on("data", stdErrWriter);
+      if (stdErrWriter) {
+        child.stderr.on("data", stdErrWriter);
+      }
+    } catch (e) {
+      reject(`Unable to spawn process: ${e}`);
     }
   });
-};
+}
 
 module.exports = spawn;
