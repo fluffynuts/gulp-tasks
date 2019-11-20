e4fc4adbdb281d4c1faff41c0c19351928bb821b
Davyd McColl <davydm@gmail.com>
2019-11-20 09:17:23 +0200

    :tada: add nuget-push since pushing with dotnet core requires the extra 'nuget' parameter :/

---
+++ modules/nuget-push.js
@@ -0,0 +1,32 @@
+const
+  spawn = require("./spawn"),
+  splitPath = require("./split-path"),
+  env = require("./env"),
+  findLocalNuget = require("./find-local-nuget");
+
+function isDotnetCore(binaryPath) {
+  const
+    parts = splitPath(binaryPath),
+    executable = (parts[parts.length-1] || "");
+  return !!executable.match(/^dotnet(:?\.exe)?$/i);
+}
+
+async function nugetPush(packageFile, sourceName) {
+  const
+    nuget = await findLocalNuget(),
+    args = [ "push", packageFile, "-Source", sourceName || "nuget.org" ];
+  if (isDotnetCore(nuget)) {
+    // ffs dotnet core breaks things that used to be simple
+    // -> _some_ nuget commands require 'dotnet nuget ...'
+    // -> _others_ don't, eg 'dotnet restore'
+    args.splice(0, 0, "nuget");
+  }
+  if (env.resolveFlag("DRY_RUN")) {
+    console.log(`nuget publish dry run: ${nuget} ${args.join(" ")}`);
+    return;
+  }
+  console.log(`pushing package ${packageFile}`);
+  return spawn(nuget, args);
+}
+
+module.exports = nugetPush;
---
+++ modules/split-path.js
@@ -0,0 +1,3 @@
+module.exports = function splitPath(path) {
+  return (path || "").split(/\/|\\/);
+}
