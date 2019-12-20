ff75d6304de84f07916a5f385aacb91bbe229f6e
Davyd McColl <davydm@gmail.com>
2019-12-20 11:08:53 +0200

    :bug: should import the correct module :/

--- modules/resolve-git-branch.js
+++ modules/resolve-git-branch.js
@@ -1,11 +1,11 @@
 const
   env = requireModule("env"),
-  readLocalBranch = requireModule("read-local-branch");
+  readCurrentBranch = requireModule("read-current-branch");
 
 module.exports = async function() {
   const fromEnv = env.resolve("GIT_BRANCH");
   if (fromEnv) {
     return fromEnv;
   }
-  return await readLocalBranch();
+  return await readCurrentBranch();
 }
