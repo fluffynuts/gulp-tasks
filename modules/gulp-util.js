1057ef45e9c0104ceed396cd164e48ede3875167
Davyd McColl <davydm@gmail.com>
2020-05-01 16:44:24 +0200

    :lock: it's time to stop depending on vulnerable, deprecated gulp-util; this shim is now _only_ a shim

--- modules/gulp-util.js
+++ modules/gulp-util.js
@@ -1,13 +1,5 @@
-const
-  gulpVersion = requireModule("gulp-version");
-
-if (gulpVersion.major === 3) {
-    module.exports = requireModule("gulp-util");
-} else {
-    // gulp-util is deprecated in 4 :/
-    module.exports = {
-        PluginError: require("plugin-error"),
-        log: require("fancy-log"),
-        colors: require("ansi-colors")
-    }
-}
+module.exports = {
+  PluginError: require("plugin-error"),
+  log: require("fancy-log"),
+  colors: require("ansi-colors")
+};
