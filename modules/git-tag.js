82b86f5adc4c0ff942aae43dc98f511fad12fae0
Davyd McColl <davydm@gmail.com>
2020-05-16 10:07:02 +0200

    :bookmark: goes with zarro v1.36.0

--- modules/git-tag.js
+++ modules/git-tag.js
@@ -1,9 +1,9 @@
 const
   Git = require("simple-git/promise"),
-  gutil = requireModule("gulp-util"),
-  git = new Git();
+  gutil = requireModule("gulp-util");
 
-module.exports = async function gitTag(tag, comment) {
+module.exports = async function gitTag(tag, comment, where) {
+  const git = new Git(where);
   gutil.log(gutil.colors.cyan(`Tagging at: "${tag}"`));
   comment = comment || `:bookmark: tagging at ${tag}`;
   await git.addAnnotatedTag(tag, comment);
