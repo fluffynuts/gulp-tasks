acf18c366281c35c1950d8f40667a952f21fbbf8
Davyd McColl <davydm@gmail.com>
2020-09-10 11:18:49 +0200

    :lock: upgrade yargs, should resolve warnings about outdated yargs-parser

--- modules/version-reading-shared.js
+++ modules/version-reading-shared.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.tryReadVersionFrom = void 0;
 function readTextFrom(node) {
     return node
         ? node[0]
