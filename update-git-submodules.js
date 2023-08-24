8653e6eefcb7aa769945f325b05a7eaca5e1c994
Davyd McColl <davydm@gmail.com>
2023-08-24 12:36:37 +0200

    :bookmark: goes with zarro v1.144.92

--- update-git-submodules.js
+++ update-git-submodules.js
@@ -1,11 +1,11 @@
 "use strict";
 (function () {
-    const gulp = requireModule("gulp"), status = requireModule("status"), gitFactory = require("simple-git"), env = requireModule("env");
+    const gulp = requireModule("gulp"), env = requireModule("env");
     env.associate("UPDATE_SUBMODULES_TO_LATEST", "update-git-submodules");
     gulp.task("update-git-submodules", "Updates all git submodules to latest commit on master branch", async () => {
-        const git = gitFactory(".");
-        await status.run("Ensure submodules are initialized...", async () => await git.subModule(["update", "--init"]));
-        await status.run("Check out master on all submodules...", async () => await git.subModule(["foreach", "git", "checkout", "master"]));
-        await status.run("Update to latest commit on each submodule...", async () => await git.subModule(["foreach", "git", "pull", "--rebase"]));
+        const { ExecStepContext } = require("exec-step"), ctx = new ExecStepContext(), gitFactory = require("simple-git"), git = gitFactory(".");
+        await ctx.run("Ensure submodules are initialized...", async () => await git.subModule(["update", "--init"]));
+        await ctx.run("Check out master on all submodules...", async () => await git.subModule(["foreach", "git", "checkout", "master"]));
+        await ctx.run("Update to latest commit on each submodule...", async () => await git.subModule(["foreach", "git", "pull", "--rebase"]));
     });
 })();
