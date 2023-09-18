9b70dc6cb2dbbf990b927fd59b8141c6762b7314
Davyd McColl <davydm@gmail.com>
2023-09-18 10:30:25 +0200

    :sparkles: add builder for SystemResult

--- modules/system-result.js
+++ modules/system-result.js
@@ -1,5 +1,47 @@
 "use strict";
 (function () {
+    function splitIfNecessary(data) {
+        if (Array.isArray(data)) {
+            return [...data];
+        }
+        return data.split('\n')
+            .map(line => line.replace(/\r$/, ""));
+    }
+    class SystemResultBuilder {
+        constructor() {
+            this._exe = "";
+            this._args = [];
+            this._exitCode = 0;
+            this._stderr = [];
+            this._stdout = [];
+            this._mutators = [];
+        }
+        withExe(exe) {
+            return this.with(o => o._exe = exe);
+        }
+        withArgs(args) {
+            return this.with(o => o._args = [...args]);
+        }
+        withExitCode(exitCode) {
+            return this.with(o => o._exitCode = exitCode);
+        }
+        withStdErr(stderr) {
+            return this.with(o => o._stderr = splitIfNecessary(stderr));
+        }
+        withStdOut(stdout) {
+            return this.with(o => o._stdout = splitIfNecessary(stdout));
+        }
+        with(mutator) {
+            this._mutators.push(mutator);
+            return this;
+        }
+        build() {
+            for (const mutator of this._mutators) {
+                mutator(this);
+            }
+            return new SystemResult(this._exe, this._args, this._exitCode, this._stderr, this._stdout);
+        }
+    }
     class SystemResult {
         constructor(exe, args, exitCode, stderr, stdout) {
             this.exe = exe;
@@ -7,6 +49,9 @@
             this.exitCode = exitCode;
             this.stderr = stderr;
             this.stdout = stdout;
+        }
+        static create() {
+            return new SystemResultBuilder;
         }
         isResult() {
             return true;
--- modules/system-result.ts
+++ modules/system-result.ts
@@ -1,4 +1,71 @@
 (function() {
+  function splitIfNecessary(
+    data: string[] | string
+  ): string[] {
+    if (Array.isArray(data)) {
+      return [ ...data];
+    }
+    return data.split('\n')
+      .map(line => line.replace(/\r$/, ""));
+  }
+
+  class SystemResultBuilder {
+    private _exe: string = "";
+    private _args: string[] = [];
+    private _exitCode: number = 0;
+    private _stderr: string[] = [];
+    private _stdout: string[] = [];
+
+    private _mutators = [] as ((o: SystemResultBuilder) => void)[];
+
+    public withExe(exe: string): SystemResultBuilder {
+      return this.with(
+        o => o._exe = exe
+      );
+    }
+
+    public withArgs(args: string[]): SystemResultBuilder {
+      return this.with(
+        o => o._args = [ ...args ]
+      );
+    }
+
+    public withExitCode(exitCode: number): SystemResultBuilder {
+      return this.with(
+        o => o._exitCode = exitCode
+      );
+    }
+
+    public withStdErr(stderr: string[]): SystemResultBuilder {
+      return this.with(
+        o => o._stderr = splitIfNecessary(stderr)
+      );
+    }
+
+    public withStdOut(stdout: string[]): SystemResultBuilder {
+      return this.with(
+        o => o._stdout = splitIfNecessary(stdout)
+      );
+    }
+
+    private with(mutator: (o: SystemResultBuilder) => void): SystemResultBuilder {
+      this._mutators.push(mutator);
+      return this;
+    }
+
+    public build(): SystemResult {
+      for (const mutator of this._mutators) {
+        mutator(this);
+      }
+      return new SystemResult(
+        this._exe,
+        this._args,
+        this._exitCode,
+        this._stderr,
+        this._stdout
+      );
+    }
+  }
   class SystemResult {
     constructor(
       public exe: string,
@@ -9,11 +76,15 @@
     ) {
     }
 
-    isResult(): this is SystemResult {
+    public static create(): SystemResultBuilder {
+      return new SystemResultBuilder;
+    }
+
+    public isResult(): this is SystemResult {
       return true;
     }
 
-    static isResult(o: any): o is SystemResult {
+    public static isResult(o: any): o is SystemResult {
       return o instanceof SystemResult;
     }
   }
